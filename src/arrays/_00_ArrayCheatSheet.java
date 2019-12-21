be276c260e37ef298b42312dd8add1422bf0ea0b
League Student <league@iMac-5.attlocal.net>
2019-12-21 11:26:52 -0800

    -_-

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,23 +1,32 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String [] things = {"hedgehog", "papaya","ribbon", "beaker", "hyperbola"};
 		//2. print the third element in the array
-		
+		System.out.println(things[2]);
 		//3. set the third element to a different value
-		
+		things[2] = "tulle";
 		//4. print the third element again
-		
+		System.out.println(things[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for (int i=0;i<things.length;i++) {
+		     System.out.println(things[i]);
+		}
 		
 		//6. make an array of 50 integers
-		
+		int[] integers = new int[50];
 		//7. use a for loop to make every value of the integer array a random number
 		
+		for (int n : integers) {
+			Random number = new Random();
+			int r = number.nextInt();
+		     System.out.println(r);
+		}
 		//8. without printing the entire array, print only the smallest number in the array
 		
 		//9 print the entire array to see if step 8 was correct
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,10 +1,13 @@
 package arrays;
 
+import java.awt.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
-
+public static void main(String[] args) {
+	
 	//2. create an array of 5 robots.
-
+	
 	//3. use a for loop to initialize the robots.
 
 		//4. make each robot start at the bottom of the screen, side by side, facing up
@@ -19,4 +22,5 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
 }
